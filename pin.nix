4e3c9e12b4438b86aa75fd1d661485298d1a74c0
Josh Gustafson <j@gustafson.me>
2026-06-14 12:15:24 -0600

    build fix

--- pin.nix
+++ pin.nix
@@ -3,5 +3,5 @@
   version = "0.1.464-beta";
   sourceRev = "b32ef2ef865eb89ce5e9075e84e5e35832c91a5f";
   sourceHash = "sha256-TuUK/n+6bWbaq8Lk8Un2j9GCz8TI+hcZLZF+ls3xe1k=";
-  npmDepsHash = "sha256-HMivlNRgyDP3jVlb2nLMz/q+h9qah0YsXvk0J2PYlsQ=";
+  npmDepsHash = "sha256-HISpFHq798aoavnAibDMyrv+DCDS1kMpUolJvvjoyik=";
 }
