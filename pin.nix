64001ccc4fc16e956437616c1bf88d5bc97c183a
Josh Gustafson <j@gustafson.me>
2026-08-29 17:41:29 -0600

    Migrate dependency selections to pin.nix

--- pin.nix
+++ pin.nix
@@ -4,4 +4,20 @@
   sourceRev = "85b49eeb565fe871679e681f542581611fb45ba0";
   sourceHash = "sha256-FIrDe9Br78736SKPhBS/WqpTuwI/MwqhXCQx5qGHCII=";
   npmDepsHash = "sha256-qzl0xsxSwfxJGKlSJaidS6hHnSygBUDJL2RkToH98k4=";
+  dependencies = {
+    "typer" = "main";
+    "fastapi" = "v0.141.1";
+    "uvicorn" = "v0.52.1";
+    "pydantic" = "main";
+    "packaging" = "main";
+    "matplotlib" = "v3.10.9";
+    "pandas" = "v2.3.3";
+    "datasets" = "v4.3";
+    "ddgs" = "v9.14.4";
+    "gguf" = "v0.19.0";
+    "sqlite-vec" = "v0.1.9";
+    "diffusers" = "main";
+    "transformers" = "v5.5.0";
+    "unsloth" = "main";
+  };
 }
